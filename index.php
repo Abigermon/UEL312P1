21367046976d979095a6dc16770151c6fea1a25b
Hugo <hugorytlewski10@gmail.com>
2024-11-26 09:01:52 +0100

    fix inded.php and add template base with style

--- index.php
+++ index.php
@@ -1,23 +1,11 @@
-<?php declare(strict_types=1);
 
-require_once __DIR__ . '/vendor/autoload.php'; // Assurez-vous que le chemin est correct
-
-use Framework312\Router\SimpleRouter;
-use Framework312\Template\SimpleRenderer; // Utilisez l'implémentation concrète
-use Framework312\Router\View\AccueilView;
-use Twig\Loader\FilesystemLoader;
+<?php
 use Twig\Environment;
+use Twig\Loader\FilesystemLoader;
 
-// Initialisation du moteur de templates (Twig)
-$loader = new FilesystemLoader(__DIR__ . '/src/template'); // Assurez-vous que le chemin est correct
+require_once 'vendor/autoload.php';
+
+$loader = new FilesystemLoader('src/Template/');
 $twig = new Environment($loader);
 
-// Initialisation du routeur
-$router = new SimpleRouter(new SimpleRenderer($twig)); // Utilisez l'implémentation concrète
-
-// Enregistrement de la route pour la page d'accueil
-$router->register('/', AccueilView::class); // Route pour le chemin '/'
-
-// Servir la requête
-$router->serve();
-?>
+echo $twig->render('index.twig', ['auteurs' => 'Abigail Germon , Vincent Ackermann et Hugo Rytlewski']);
