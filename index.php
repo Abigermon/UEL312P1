e3525bbab5bd86f995c4019edb1eb0ac244bef37
Hugo <hugorytlewski10@gmail.com>
2024-11-26 09:04:44 +0100

    add alert

--- index.php
+++ index.php
@@ -8,4 +8,4 @@ require_once 'vendor/autoload.php';
 $loader = new FilesystemLoader('src/Template/');
 $twig = new Environment($loader);
 
-echo $twig->render('index.twig', ['auteurs' => 'Abigail Germon , Vincent Ackermann et Hugo Rytlewski']);
+echo $twig->render('index.twig', ['auteurs' => 'Abigail Germon , Vincent Ackermann, Hillel Magnichewer et Hugo Rytlewski']);
